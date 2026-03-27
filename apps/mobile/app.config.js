e5233a5c70c39f7236368d611c37a0722c6adb7e
kerk jazul <kerkjazul@kerks-MacBook-Air.local>
2026-03-27 15:30:43 +0800

    chore: bump version to 1.0.9

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: "Bookfelt",
     slug: "bookfelt",
-    version: "1.0.8",
+    version: "1.0.9",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "bookfelt",
